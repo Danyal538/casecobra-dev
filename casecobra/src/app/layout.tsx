8232c46a5eec59e78f66bbd1d23b2b3c6c154700
Danyal538 <danyalatique0@gmail.com>
2025-10-16 00:32:29 +0500

    Working on Image Upload page

--- casecobra/src/app/layout.tsx
+++ casecobra/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
-        {children}
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            {children}
+          </div>
+          <Footer />
+        </main>
+        <Toaster />
       </body>
     </html>
   );
